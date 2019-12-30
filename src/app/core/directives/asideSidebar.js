cc0ff3b1086564c646c99a266a20dc78990237c9
mmso <mmso@mmso.se>
2019-12-30 15:45:01 +0100

    Show calendar app for all users

--- src/app/core/directives/asideSidebar.js
+++ src/app/core/directives/asideSidebar.js
@@ -1,13 +1,11 @@
 /* @ngInject */
-function asideSidebar(userType) {
+function asideSidebar() {
     return {
         replace: true,
         restrict: 'E',
         templateUrl: require('../../../templates/layout/asideSidebar.tpl.html'),
         scope: {},
-        link(scope) {
-            scope.hasCalendar = userType().isPaid;
-        }
+        link() {}
     };
 }
 
