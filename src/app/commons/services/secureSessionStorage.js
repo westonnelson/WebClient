1daa3e48baac810b03d93ebf020354619fe62583
mmso <mmso@mmso.se>
2020-03-06 09:45:10 +0100

    Use pagehide instead of unload

--- src/app/commons/services/secureSessionStorage.js
+++ src/app/commons/services/secureSessionStorage.js
@@ -1,12 +1,37 @@
-import createSecureSessionStorage from 'proton-shared/lib/secureSessionStorage';
+import { load, save } from 'proton-shared/lib/helpers/secureSessionStorage';
+import createStore from 'proton-shared/lib/helpers/store';
 import { MAILBOX_PASSWORD_KEY, UID_KEY } from 'proton-shared/lib/constants';
+
 import { EO_DECRYPTED_TOKEN_KEY, EO_PASSWORD_KEY } from '../../constants';
 
 const SECURE_SESSION_STORAGE_KEYS = [MAILBOX_PASSWORD_KEY, UID_KEY, EO_DECRYPTED_TOKEN_KEY, EO_PASSWORD_KEY];
 
 /* @ngInject */
 function secureSessionStorage() {
-    return createSecureSessionStorage(SECURE_SESSION_STORAGE_KEYS);
+    const store = createStore(load(SECURE_SESSION_STORAGE_KEYS));
+
+    if ('onpagehide' in window) {
+        const handlePageShow = () => {
+            // This does not need to do anything. The main purpose is just to reset window.name and sessionStorage to fix the Safari bug described below
+            load(SECURE_SESSION_STORAGE_KEYS);
+        };
+
+        const handlePageHide = () => {
+            // Cannot use !event.persisted because Safari 13.1 does not send that when you are navigating on the same domain
+            save(SECURE_SESSION_STORAGE_KEYS, store.getState());
+        };
+
+        window.addEventListener('pageshow', handlePageShow, true);
+        window.addEventListener('pagehide', handlePageHide, true);
+    } else {
+        const handleUnload = () => {
+            save(SECURE_SESSION_STORAGE_KEYS, store.getState());
+        };
+
+        window.addEventListener('unload', handleUnload, true);
+    }
+
+    return store;
 }
 
 export default secureSessionStorage;
