a1454db0937924c150628f14d02e34886ec7955c
Matthieu Bué <matthieu.bue@protonmail.com>
2020-06-10 16:16:04 +0000

    Feat/coreweb 201

--- src/app/ui/directives/appsDropdown.js
+++ src/app/ui/directives/appsDropdown.js
@@ -1,10 +1,17 @@
 /* @ngInject */
-function appsDropdown() {
+function appsDropdown($filter, authentication) {
+    const humanSize = $filter('humanSize');
     return {
         scope: {},
         replace: true,
         restrict: 'E',
-        templateUrl: require('../../../templates/ui/appsDropdown.tpl.html')
+        templateUrl: require('../../../templates/ui/appsDropdown.tpl.html'),
+        link(scope) {
+            const { UsedSpace, MaxSpace } = authentication.user;
+            const percentage = Math.round((UsedSpace * 100) / MaxSpace);
+            scope.spacePercentage = isNaN(percentage) ? 0 : percentage;
+            scope.spaceHuman = `${humanSize(UsedSpace)} / ${humanSize(MaxSpace)}`;
+        }
     };
 }
 export default appsDropdown;
